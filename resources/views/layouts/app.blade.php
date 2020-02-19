b0d0e94c8b88b09fd6a854360ca72b0735b22871
josepaulodelacruz <whoseepowlu@gmail.com>
2020-02-19 12:37:48 +0800

    trix editor and flatpickr

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,6 +17,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('css')
+
+
 </head>
 <body>
     <div id="app">
@@ -114,11 +116,9 @@
         </main>
     </div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" crossorigin="anonymous"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 
-@yield('scripts')
+    @yield('scripts')
 
 </body>
 </html>
